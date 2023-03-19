23ba52383ad0eacbf8c702b3448df3bc790ab77e
Szymon-PC <wachim17@gmail.com>
2023-03-19 16:37:29 +0100

    some st00bit tearing done

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from PIL import Image
+import PIL
+import glob
 import numpy as np
 import matplotlib.pyplot as plt
+from os import listdir
+from os.path import isfile, join
+
 # equation: f(x) = sin(n * x + m * pi/10), m = -M/2, ... , M/2, n = 3, 
 # for M = 64
-
-
-
 
 
 def genarateImage(path, M, _n):
@@ -23,10 +26,51 @@ def genarateImage(path, M, _n):
     plt.close()
 
 
+# generate image sequence
+# X = np.arange(-30, 30)
+# i = 0
+wings = 3
+directoryPath = str(wings) + "n propeller\\"
+# for x in X:
+#     path = ""
+#     if i < 10:
+#         path = directoryPath + "0" + str(i) + ".png"
+#     else:
+#         path = directoryPath + str(i) + ".png"
+#     genarateImage(path, x, wings)
+#     i += 1
 
-X = np.arange(-32, 33)
-i = 0
-for x in X:
-    path = "5n propeller\\" + str(i) + ".png"
-    genarateImage(path, x, 5)
-    i += 1
+
+
+# Create the Gif
+frames = []
+imgs = [directoryPath + f for f in listdir(directoryPath) if isfile(join(directoryPath, f))]
+# print(imgs)
+
+# for i in imgs:
+#     new_frame = Image.open(i)
+#     frames.append(new_frame)
+
+# frames[0].save(str(wings) + 'n.gif', format='GIF',
+#                append_images=frames[1:],
+#                save_all=True,
+#                duration=50, loop=0)
+
+
+
+# all images as 3D arrays in list 
+imagesList = [np.array(Image.open(img)) for img in imgs]
+
+source = imagesList[1].copy()
+targetImage = imagesList[0]
+
+print(targetImage.shape)
+
+#for i in range(256):
+targetImage[:][0:150][:] = source[:][0:150][:]
+
+im_from_array = PIL.Image.fromarray(np.uint8(targetImage))
+im_from_array.show()
+
+
+
