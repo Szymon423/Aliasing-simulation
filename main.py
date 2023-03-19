b3e59552e4fe414cf83d1ba2d049bc8f1c24de19
Szymon-PC <wachim17@gmail.com>
2023-03-19 19:51:04 +0100

    working kurwa

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def genarateImage(path, M, _n):
     X = np.linspace(0, 2*np.pi, 10000)
     Y = f(X, _n, M)
 
-    plt.figure(figsize=(1, 1), dpi=256)
+    plt.figure(figsize=(1, 1), dpi=240)
     plt.polar(X, Y)
     plt.grid(False)
     plt.box(False)
@@ -27,18 +27,18 @@ def genarateImage(path, M, _n):
 
 
 # generate image sequence
-# X = np.arange(-30, 30)
-# i = 0
+X = np.arange(-30, 30)
+i = 0
 wings = 3
 directoryPath = str(wings) + "n propeller\\"
-# for x in X:
-#     path = ""
-#     if i < 10:
-#         path = directoryPath + "0" + str(i) + ".png"
-#     else:
-#         path = directoryPath + str(i) + ".png"
-#     genarateImage(path, x, wings)
-#     i += 1
+for x in X:
+    path = ""
+    if i < 10:
+        path = directoryPath + "0" + str(i) + ".png"
+    else:
+        path = directoryPath + str(i) + ".png"
+    genarateImage(path, x, wings)
+    i += 1
 
 
 
@@ -47,30 +47,52 @@ frames = []
 imgs = [directoryPath + f for f in listdir(directoryPath) if isfile(join(directoryPath, f))]
 # print(imgs)
 
-# for i in imgs:
-#     new_frame = Image.open(i)
-#     frames.append(new_frame)
+for i in imgs:
+    new_frame = Image.open(i)
+    frames.append(new_frame)
 
-# frames[0].save(str(wings) + 'n.gif', format='GIF',
-#                append_images=frames[1:],
-#                save_all=True,
-#                duration=50, loop=0)
+frames[0].save(str(wings) + 'n.gif', format='GIF',
+               append_images=frames[1:],
+               save_all=True,
+               duration=50, loop=0)
 
 
 
 # all images as 3D arrays in list 
 imagesList = [np.array(Image.open(img)) for img in imgs]
 
-source = imagesList[1].copy()
-targetImage = imagesList[0]
+mod_directory = "mod 3n propeller\\"
 
-print(targetImage.shape)
+height = 240
+div = 10
 
-#for i in range(256):
-targetImage[:][0:150][:] = source[:][0:150][:]
+for j in range(59):
+    current = imagesList[j + 1].copy()
+    for i in range((height // div) - 1):
+        previous = imagesList[(i + j) % 60].copy()
+        current[:][div * i: div * (i + 1)][:] = previous[:][div * i: div * (i + 1)][:]
+        
 
-im_from_array = PIL.Image.fromarray(np.uint8(targetImage))
-im_from_array.show()
+    im_from_array = PIL.Image.fromarray(np.uint8(current))
+    name = ""
+    if j < 10:
+        name = mod_directory + "0" + str(j) + ".png"
+    else:
+        name = mod_directory + str(j) + ".png"
+    im_from_array.save(name)
 
 
+    
+# Create the Gif 2
+frames = []
+imgs = [mod_directory + f for f in listdir(mod_directory) if isfile(join(mod_directory, f))]
+# print(imgs)
 
+for i in imgs:
+    new_frame = Image.open(i)
+    frames.append(new_frame)
+
+frames[0].save(str(wings) + 'n_mod.gif', format='GIF',
+               append_images=frames[1:],
+               save_all=True,
+               duration=50, loop=0)
